9aa2df973a15c445b7b4cba310c812675e9c5ddf
Ilya Kushlianski <kushliansky@gmail.com>
2020-02-26 01:12:00 +0300

    Enable cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors({
+    // in reality should be kinda FRONTEND_URL from .env
+    origin: '*',
+  });
+
+  await app.listen(3001);
 }
 bootstrap();
