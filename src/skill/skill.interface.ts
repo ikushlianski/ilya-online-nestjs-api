78c5585fcb1404e37605d3d85d22601e01e244e3
Ilya Kushlianski <kushliansky@gmail.com>
2020-02-27 23:42:14 +0300

    Update skill model and interface

--- src/skill/skill.interface.ts
+++ src/skill/skill.interface.ts
@@ -1,7 +1,18 @@
 import { Document } from 'mongoose';
 
+type SkillGroups = 'frontend' | 'backend' | 'devTools' | 'misc' | 'future';
+
 export interface Skill extends Document {
   readonly name: string;
   readonly description: string;
+  readonly group: SkillGroups;
+  readonly order: 1 | 2 | 3;
   readonly lastUsed: number;
+  readonly frequency: string;
+  readonly hot: boolean;
+  readonly top: boolean;
+  readonly workedWith: Array<string>;
+  readonly yetToLearn: Array<string>;
+  readonly image: string;
+  readonly cv: boolean;
 }
--- src/skill/skill.schema.ts
+++ src/skill/skill.schema.ts
@@ -3,5 +3,14 @@ import * as mongoose from 'mongoose';
 export const SkillSchema = new mongoose.Schema({
   name: String,
   description: String,
+  group: String,
+  order: Number,
   lastUsed: Number,
+  frequency: String,
+  hot: Boolean,
+  top: Boolean,
+  workedWith: Array,
+  yetToLearn: Array,
+  image: String,
+  cv: Boolean,
 });
