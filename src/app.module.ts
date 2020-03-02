c4bb73880f6e9dcdb6eaed27450a33ac27582dbc
Ilya Kushlianski <kushliansky@gmail.com>
2020-03-03 00:45:49 +0300

    Remove unused imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SkillModule } from './skill';
-import { databaseProviders } from './db/database.providers';
 import { DatabaseModule } from './db/database.module';
 import { JobModule } from './job';
 
